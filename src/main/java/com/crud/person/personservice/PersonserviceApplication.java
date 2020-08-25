84221aafb9c924e5e79f523df69f0e7dcdf9c597
judavid <judavid.arias@gmail.com>
2020-08-25 18:54:34 -0500

    test

---
+++ src/main/java/com/crud/person/personservice/PersonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.crud.person.personservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonserviceApplication.class, args);
+	}
+
+}
